e9c9982706b8c145bc83f15af1e5578583e2ac82
Justyna Kuchta <justyna.kuchta@gmail.com>
2019-06-23 18:50:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// add solution here
+var musicians = ["John Lennon", "Ringo Star", "Paul McCartney"];
+var instruments = ["piano", "drums", "guitar"];
+
+function theBeatlesPlay(musicians, instruments) {
+  var whoPlaysWhat = [];
+  for(var i = 0; i < musicians.length; i++) {
+    whoPlaysWhat.push(musicians[i] + " plays " + instruments[i])
+  }
+  return whoPlaysWhat;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  } while(incrementVariable() < facts.length) {
+    facts = facts[i] + " !!!"
+  }
+  return facts;
+}
---
+++ lala.js
@@ -0,0 +1,15 @@
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while(i < facts.length) {
+    facts = facts[i] + " !!!"
+  }
+  return facts;
+}
+console.log(johnLennonFacts(facts))
